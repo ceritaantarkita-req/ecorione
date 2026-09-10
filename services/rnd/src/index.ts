9ca2a8dfd5abca6e609488401e020e10507c632b
ceritaantarkita-req <ceritaantarkita@gmail.com>
2026-09-10 08:59:17 +0700

    feat: export RnD backup adapter

--- services/rnd/src/index.ts
+++ services/rnd/src/index.ts
@@ -9,5 +9,6 @@
 export * from "./db.js";
 export * from "./store.js";
 export * from "./dataset.js";
+export * from "./backup.js";
 export * from "./http.js";
 export type { SqliteDatabase } from "./sqlite.js";
