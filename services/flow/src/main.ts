460de241df10e2ab58b653138882f6d4ceae2cd3
ceritaantarkita-req <ceritaantarkita@gmail.com>
2026-09-19 14:54:53 +0700

    feat(pe-04): wire RnD owner into Flow Run projection

--- services/flow/src/main.ts
+++ services/flow/src/main.ts
@@ -12,6 +12,7 @@ const port = Number(process.env.ECORIONE_FLOW_PORT ?? "17028");
 const temporalAddress = process.env.ECORIONE_TEMPORAL_ADDRESS ?? "127.0.0.1:7233";
 const temporalNamespace = process.env.ECORIONE_TEMPORAL_NAMESPACE ?? "default";
 const hubUrl = process.env.ECORIONE_HUB_URL ?? "http://127.0.0.1:17024";
+const rndUrl = process.env.ECORIONE_RND_URL ?? "http://127.0.0.1:17021";
 const token = process.env.ECORIONE_INTERNAL_TOKEN || undefined;
 const dbPath = resolveRepoRuntimePath(
   REPO_ROOT,
@@ -28,6 +29,7 @@ const triggerRepository = new TriggerRepository(db);
 await reconcilePersistedTimeTriggers(triggerRepository, graphRepository, temporal);
 const app = buildFlowServer(temporal, {
   hubUrl,
+  rndUrl,
   token,
   logger: true,
   graphRepository,
